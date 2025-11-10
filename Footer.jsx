f37c503b9505392270d6bd02dacb634b76df838e
zaineb <zaineb.asses@isgb.ucar.tn>
2025-11-10 17:54:24 +0100

    Add files via upload

--- Footer.jsx
+++ Footer.jsx
@@ -3,20 +3,20 @@ import React from "react";
 export default function Footer() {
   return (
     <footer style={{
-      backgroundColor: "#333",
+      backgroundColor: "#1d0f0fff",
       color: "white",
       textAlign: "center",
       padding: "1rem",
       marginTop: "3rem"
     }}>
-      <p>2025 Zaineb Asses A ISGB</p>
+      <p> Zaineb Asses  etudiante A ISGB</p>
       <p>
-        Contact : <a href="Zeynebasses@gmail.com" style={{ color: "white" }}>mon mail </a>
+        Contact: <a href="https://mail.google.com/mail/u/0/?hl=fr#inbox" style={{ color: "white" }}>mon mail </a>
       </p>
       <p>
         Réseaux sociaux : 
-        <a href ="zeynebasses@gmail.com">envoyer moi un email </a>
-        <a href=" https://github.com/">mon compte github</a> | 
+        <a href ="https://mail.google.com/mail/u/1/#inbox?compose=new">envoyer moi un email </a>
+        <a href=" https://github.com/">mon compte github</a> 
         
       </p>
     </footer>
